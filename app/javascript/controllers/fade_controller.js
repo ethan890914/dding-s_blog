60213a8d34b3acaa34b5569d81103f9b6b8a0931
ethan890914 <ethan516858@gmail.com>
2022-07-16 16:37:07 +0800

    finish About page and add some js/css

---
+++ app/javascript/controllers/fade_controller.js
@@ -0,0 +1,44 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.element.textContent = "Fade out!"
+  }
+}
+
+// import { Controller } from "@hotwired/stimulus"
+// $(window).on("load",function() {
+//   $(window).scroll(function() {
+//     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+//     $(".fade").each(function() {
+//       /* Check the location of each desired element */
+//       var objectBottom = $(this).offset().top + $(this).outerHeight();
+      
+//       /* If the element is completely within bounds of the window, fade it in */
+//       if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+//         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+//       } else { //object goes out of view (scrolling up)
+//         if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+//       }
+//     });
+//   }).scroll(); //invoke scroll-handler on page-load
+// });
+// export default class extends Controller {
+//   $(window).on("load",function() {
+//     $(window).scroll(function() {
+//       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+//       $(".fade").each(function() {
+//         /* Check the location of each desired element */
+//         var objectBottom = $(this).offset().top + $(this).outerHeight();
+        
+//         /* If the element is completely within bounds of the window, fade it in */
+//         if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+//           if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+//         } else { //object goes out of view (scrolling up)
+//           if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+//         }
+//       });
+//     }).scroll(); //invoke scroll-handler on page-load
+//   });
+// }
+
