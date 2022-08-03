509f9f9371c8e283e6bafd89980eddbed576642e
ethan890914 <ethan516858@gmail.com>
2022-08-03 14:27:54 +0800

    add feature: pick today's quote at HomePage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
     def index
-    
+        quotes = Quote.all.ids
+        idx = Date.today.day % quotes.length
+        @quote = Quote.find(quotes[idx])
     end
 
     def about
