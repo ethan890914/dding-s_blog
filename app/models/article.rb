3ace78451b48213fb46c8e6dc6d84ba1269728dd
ethan890914 <ethan516858@gmail.com>
2022-08-09 22:04:00 +0800

    prettify: article show page

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,5 @@ class Article < ApplicationRecord
     validates :body, presence: true, length: {minimum:1}
     # validates :review, presence: true
     acts_as_taggable_on :tags
-    has_rich_text :review
+    # has_rich_text :review
 end
